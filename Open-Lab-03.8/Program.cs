8fe928898f88a65fb708e2b884a1c3081283fd6d
MartinNovysedlak <martinnovysedlak48@gmail.com>
2019-10-07 22:17:36 +0200

    Initial

---
+++ Open-Lab-03.8/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Open_Lab_03._8
+{
+    class Program
+    {
+        public static bool Isplural(string Word)
+        {
+            return Word.EndsWith("s");
+        }
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            Console.WriteLine(Isplural(word));
+        }
+
+    }
+}
